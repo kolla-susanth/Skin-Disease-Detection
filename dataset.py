8aeef8ef53f345227ae361f4f5cde7405eac4dcc
kolla-susanth <134594795+kolla-susanth@users.noreply.github.com>
2025-05-23 22:12:50 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,5 +1,5 @@
 from roboflow import Roboflow
-rf = Roboflow(api_key="qDw7LTKhpnlGOw81ntqH")
+rf = Roboflow(api_key="")  # add api that is used in roboflow for data anotation 
 project = rf.workspace("skin-diseases-jzde4").project("skin-diseases-i30ay")
 version = project.version(1)
 dataset = version.download("yolov8")
